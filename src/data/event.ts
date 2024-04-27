01b74b5239afefe8772a87fc2fc6fca511b121c7
bernard-ng <ngandubernard@gmail.com>
2024-04-27 11:26:18 +0200

    fix country

--- src/data/event.ts
+++ src/data/event.ts
@@ -61,15 +61,15 @@ export default {
         {name: 'Bernard Ng', role: 'Hackaton Organizer | Technical support', organization: 'Université Nouveaux Horizons', image: '/organizers/648adf11aa06a336722320.jpg', link: 'https://www.linkedin.com/in/bernard-ngandu/', country: 'cd'},
     ],
     speakers: [
-        {name: 'Antoine Bagula, PhD', role: 'Speaker', organization: 'University of Western Cape', image: '/organizers/634d5fed05733262169654.jpg', link: 'https://www.linkedin.com/in/antoine-bagula-2075305/?originalSubdomain=za', country: 'sa'},
-        {name: 'Meshia Cédric Oveneke, PhD', role: 'Speaker', organization: 'Neotex.ai', image: '/organizers/64355d8597a5c676189023.jpeg', link: 'https://www.linkedin.com/in/meshiacedricoveneke/?originalSubdomain=be', country: 'sa'},
+        {name: 'Antoine Bagula, PhD', role: 'Speaker', organization: 'University of Western Cape', image: '/organizers/634d5fed05733262169654.jpg', link: 'https://www.linkedin.com/in/antoine-bagula-2075305/?originalSubdomain=za', country: 'za'},
+        {name: 'Meshia Cédric Oveneke, PhD', role: 'Speaker', organization: 'Neotex.ai', image: '/organizers/64355d8597a5c676189023.jpeg', link: 'https://www.linkedin.com/in/meshiacedricoveneke/?originalSubdomain=be', country: 'za'},
         {name: 'Pierre Kafunda, PhD', role: 'Speaker', organization: 'Université de Kinshasa', image: '/organizers/64465a548c5ff556470643.jpeg', link: '', country: 'cd'},
         {name: 'Patrick Mukala, PhD', role: 'Speaker', organization : 'Univesity of Wollongong in Dubai', image: '/organizers/64355fe07d732238537489.jpeg', link: 'https://www.linkedin.com/in/dr-patrick-mukala/', country: 'ae'},
         {name: 'Jordan F. Masakuna, PhD', role: 'Speaker', organization: 'University of Sherbrooke', image: '/organizers/64356049422e3998346198.jpeg', link: 'https://www.linkedin.com/in/jordan-f-masakuna-phd-194a68b4/', country: 'cd'},
         {name: 'Cyrille Kesiku', role: 'Speaker', organization: 'Univesity of DEUSTO', image: '/organizers/644481ce42378981370083.jpeg', link: 'https://www.linkedin.com/in/cyrille-kesiku-396100196/', country: 'cd'},
         {name: 'Nicanor Mayumu, PhD', role: 'Director, IndabaX DRC', organization: 'Mapon University', image: '/organizers/632f2b56cec9e786115539.jpeg', link: 'https://www.linkedin.com/in/nicanor-mayumu-500366141/', country: 'cn'},
         {name: 'Philippe Faradja, PhD', role: 'Speaker', organization: 'Université Protestante au Congo', image: '/organizers/6478da5989c38580163297.jpeg', link: 'https://www.linkedin.com/in/philippe-faradja-b974b852/', country: 'cd'},
-        {name: 'Diana Pholo', role: 'Speaker', organization: 'Predictive Insights', image: '/organizers/648620f2f3694268247135.jpeg', link: 'https://www.linkedin.com/in/diana-pholo-moanda-76ba803b/', country: 'sa'},
-        {name: 'Junior Muka', role: 'Speaker', organization: 'Woolworth South Africa', image: '/organizers/6478daaf554f3616239108.jpeg', link: 'https://www.linkedin.com/in/junior-muka-aa0b0b7/', country: 'sa'},
+        {name: 'Diana Pholo', role: 'Speaker', organization: 'Predictive Insights', image: '/organizers/648620f2f3694268247135.jpeg', link: 'https://www.linkedin.com/in/diana-pholo-moanda-76ba803b/', country: 'za'},
+        {name: 'Junior Muka', role: 'Speaker', organization: 'Woolworth South Africa', image: '/organizers/6478daaf554f3616239108.jpeg', link: 'https://www.linkedin.com/in/junior-muka-aa0b0b7/', country: 'za'},
     ]
 }
